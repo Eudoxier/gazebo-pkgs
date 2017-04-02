674f4a17e4e5bae17e14083171eceb39216be500
Immanuel Löwenstein <immanuel.loewenstein@campus.tu-berlin.de>
2017-04-03 00:03:42 +0200

    [cuboid_spawner] has now parmeter r for ration between x- and y-dimension of cuboid

--- gazebo_test_tools/src/cuboid_spawner_node.cpp
+++ gazebo_test_tools/src/cuboid_spawner_node.cpp
@@ -6,7 +6,7 @@ int main(int argc, char** argv) {
 
     if (argc < 5)
     {
-        ROS_INFO("Usage: %s <name> [x y z] [frame_id].",argv[0]);
+        ROS_INFO("Usage: %s <name> [x y z] [r] [frame_id].",argv[0]);
         return 0;
     }
 
@@ -19,16 +19,20 @@ int main(int argc, char** argv) {
     float x=0;
     float y=0;
     float z=0;
+    
+    float r=1;
 
     std::string frame_id="world";
     if (argc>2) x=atof(argv[2]);
     if (argc>3) y=atof(argv[3]);
     if (argc>4) z=atof(argv[4]);
-    if (argc>5) frame_id=argv[5];
+    if (argc>5) r=atof(argv[5]);
+    if (argc>6) frame_id=argv[6];
+
 
     float dim=0.05;   
     float mass=0.05;
 
-    spawner.spawnCube(name,frame_id,x,y,z,0,0,0,1,dim*2,dim,dim,mass);
+    spawner.spawnCube(name,frame_id,x,y,z,0,0,0,1,dim*r,dim,dim,mass);
     return 0;
 }
